cafe486c15f557f54c3d4c36bb7f2bf5f9812558
Joe Broach <joebroach@gmail.com>
2018-12-18 20:11:23 -0800

    working on ranked system plot

--- get_station_data.py
+++ get_station_data.py
@@ -32,59 +32,89 @@ for row in reader:
 print('{} systems in {} locations found'.format(n_sys, len(us_systems)))
 
 # Get station feed data and store
-#for loc in us_systems:
-#    for sys in us_systems[loc]:
-sysset = set(['BIKETOWN', 'Divvy', 'Citi Bike', 'Nice Ride Minnesota'])
+#sysset = set(['BIKETOWN', 'Divvy', 'Citi Bike', 'Nice Ride Minnesota'])
+sysset = None
+n_top = 9   # biggest n stations to map
+sysstats = []
+
+if not sysset:
+    syslist = []
+    for loc in [loc for loc in us_systems][0:]:
+        syslist.extend(us_systems[loc].keys())
+    sysset = set(syslist)
+
+for loc in us_systems:
+    for sys in [s for s in us_systems[loc] if s in sysset]:
+        badhost = False
+        epsg = None  # from calculated local UTM zone
+        url = us_systems[loc][sys]['Auto-Discovery URL']
+        print('{} {}'.format(sys, loc))
+        try:
+            r = requests.get(url, timeout=1).json()
+        except requests.exceptions.RequestException as e:
+            badhost = True
+        if badhost is True:
+            print('Bad response from {}'.format(url))
+        # TODO handle missing status or info urls
+        else:
+            status_url = next(item for item in r['data']['en']['feeds'] if
+                              item['name'] == 'station_status')['url']
+            info_url = next(item for item in r['data']['en']['feeds'] if
+                            item['name'] == 'station_information')['url']
+            try:
+                status = requests.get(status_url,
+                                      timeout=1).json()['data']['stations']
+            except requests.exceptions.RequestException as e:
+                badhost = True
+            if badhost is True:
+                print('Bad response from {}'.format(status_url))
+            else:
+                stations = {}
+                for station in [station for station in status if
+                                station['is_installed'] == 1]:
+                    stations[station['station_id']] = {}
+                print('...{} installed stations found'.format(len(stations)))
+                try:
+                    info = requests.get(info_url,
+                                        timeout=1).json()['data']['stations']
+                except requests.exceptions.RequestException as e:
+                    badhost = True
+                if badhost is True:
+                    print('Bad response from {}'.format(info_url))
+                else:
+                    for station in [station for station in info if
+                                    station['station_id'] in stations]:
+                        lat, lon = float(station['lat']), float(station['lon'])
+                        if not epsg:
+                            epsg = utm_from_latlng(lat, lon)
+                            print('epsg: {}'.format(epsg))
+                        x, y = pyproj.transform(pyproj.Proj(init='epsg:4326'),
+                                                pyproj.Proj(init='epsg:{}'.format(epsg)),
+                                                                                  lon, lat)
+                        stations[station['station_id']] = {'x': x, 'y': y}
+                    us_systems[loc][sys]['X'] = [v['x'] for k, v in stations.items()]
+                    us_systems[loc][sys]['Y'] = [v['y'] for k, v in stations.items()]
+                    us_systems[loc][sys]['N'] = len(stations)
+                    us_systems[loc][sys]['epsg'] = epsg
+                    sysstats.append((len(stations), loc, sys))
+
+top_sys = sorted(sysstats, reverse=True)
+print(top_sys)
 
 plt.figure(figsize=(11, 8.5))
 i = 0
-for loc in us_systems:
-    for sys in [s for s in us_systems[loc] if s in sysset]:
-        epsg = None  # from calculated local UTM zone
-        i += 1
-        url = us_systems[loc][sys]['Auto-Discovery URL']
-        print('{} {}'.format(sys, loc))
-        r = requests.get(url).json()
-        #rows = r.json()
-        #reader = csv.DictReader(rows)
-        status_url = next(item for item in r['data']['en']['feeds'] if
-                          item['name'] == 'station_status')['url']
-        info_url = next(item for item in r['data']['en']['feeds'] if
-                        item['name'] == 'station_information')['url']
-        status = requests.get(status_url).json()['data']['stations']
-        stations = {}
-        for station in [station for station in status if
-                        station['is_installed'] == 1]:
-            stations[station['station_id']] = {}
-        print('...{} installed stations found'.format(len(stations)))
-        info = requests.get(info_url).json()['data']['stations']
-        for station in [station for station in info if
-                        station['station_id'] in stations]:
-            lat, lon = float(station['lat']), float(station['lon'])
-            if not epsg:
-                epsg = utm_from_latlng(lat, lon)
-                print('epsg: {}'.format(epsg))
-            x, y = pyproj.transform(pyproj.Proj(init='epsg:4326'),
-                                    pyproj.Proj(init='epsg:{}'.format(epsg)),
-                                                                      lon, lat)
-            stations[station['station_id']] = {'x': x, 'y': y}
-        X = [v['x'] for k, v in stations.items()]
-        Y = [v['y'] for k, v in stations.items()]
+for (n, loc, sys) in top_sys[:n_top]:
+    i += 1
+    #print('{:0f} x {:0f} meters'.format(max(Y) - min(Y), max(X) - min(X)))
+    X = us_systems[loc][sys]['X']
+    Y = us_systems[loc][sys]['Y']
+    ax = plt.subplot(ceil(n_top / 3), 3, i, aspect='equal')
+    plt.scatter(X, Y, c='black', alpha=0.25)
+    xmar = (20000 - (max(X) - min(X))) / 2
+    ymar = (40000 - (max(Y) - min(Y))) / 2
+    plt.xlim(min(X) - xmar, max(X) + xmar)
+    plt.ylim(min(Y) - ymar, max(Y) + ymar)
+    ax.axis('off')
 
-        print('{:0f} x {:0f} meters'.format(max(Y) - min(Y), max(X) - min(X)))
-        ax = plt.subplot(ceil(len(sysset) / 3), 3, i, aspect='equal')
-        plt.scatter(X, Y, c='black', alpha=0.25)
-        xmar = (20000 - (max(X) - min(X))) / 2
-        ymar = (40000 - (max(Y) - min(Y))) / 2
-        plt.xlim(min(X) - xmar, max(X) + xmar)
-        plt.ylim(min(Y) - ymar, max(Y) + ymar)
-        ax.axis('off')
-
-#plt.axes().set_aspect('equal')
 plt.show()
 #plt.savefig('c:/tmp/foo.pdf')
-#(min(X) - xmar, max(X) + xmar)
-#(min(Y) - ymar, max(Y) + ymar)
-#x = [0, 1, 2]
-#y = [2, 4, 6]
-#plt.scatter(x, y)
